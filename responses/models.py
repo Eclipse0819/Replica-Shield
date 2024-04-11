d795e6fefed71e0914614cf47dc799292c4e7051
Ritvik-lab-coder <114284123+Ritvik-lab-coder@users.noreply.github.com>
2024-04-11 17:24:53 +0530

    Added classes to existing form elements

--- responses/models.py
+++ responses/models.py
@@ -11,8 +11,8 @@ class Response(models.Model):
     leader_email    = models.EmailField()
 
     member2         = models.CharField(max_length = 50)
-    member3         = models.CharField(max_length = 50, blank = True)
-    member4         = models.CharField(max_length = 50, blank = True)
+    member3         = models.CharField(max_length = 50)
+    member4         = models.CharField(max_length = 50)
 
     abstract        = models.TextField()
 
@@ -22,4 +22,13 @@ class ResponseForm(forms.ModelForm):
         model = Response
         fields = ['title', 'leader', 'leader_email', 'member2',
                   'member3', 'member4', 'abstract']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'input-field', 'id': 'title', 'placeholder': 'Project Title', 'requierd': True}),
+            'leader': forms.TextInput(attrs={'class': 'input-field', 'id': 'team-leader-email', 'placeholder': 'Name', 'requierd': True}),
+            'leader_email': forms.EmailInput(attrs={'class': 'input-field', 'id': 'team-leader-email', 'placeholder': 'Email', 'required': True}),
+            'member2': forms.TextInput(attrs={'class': 'input-field', 'id': 'project-team', 'placeholder': 'Member 2', 'required': True}),
+            'member3': forms.TextInput(attrs={'class': 'input-field', 'id': 'project-team', 'placeholder': 'Member 3', 'required': True}),
+            'member4': forms.TextInput(attrs={'class': 'input-field', 'id': 'project-team', 'placeholder': 'Member 4', 'required': True}),
+            'abstract': forms.Textarea(attrs={'class': 'textarea-field', 'id': 'abstract', 'placeholder': 'Project Abstract', 'required': True})
+        }
         
